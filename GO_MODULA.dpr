84596a379d5c56076352e8bb23b164d10b2155d3
angerp <paolo.angeretti@gestionaleopen.org>
2021-06-30 05:41:35 +0200

    11.01.01

--- GO_MODULA.dpr
+++ GO_MODULA.dpr
@@ -8,16 +8,16 @@ program GO_MODULA;
 
 
 
-{$R 'ZZVERSIONE_GO_EXE.res' '..\..\..\go_1010\src_101002\ZZVERSIONE_GO_EXE.rc'}
+{$R 'ZZVERSIONE_GO_EXE.res' '..\..\..\go_1011\src\ZZVERSIONE_GO_EXE.rc'}
 
 uses
   madExcept,
   Vcl.Forms,
-  DMARC in '..\..\..\go_1010\src_101002\DMARC.pas' {ARC: TDataModule},
-  GGLOGIN in '..\..\..\go_1010\src_101002\GGLOGIN.pas' {LOGIN},
-  GGBASE in '..\..\..\go_1010\src_101002\GGBASE.pas' {BASE},
-  GGFORMBASE in '..\..\..\go_1010\src_101002\GGFORMBASE.pas' {FORMBASE},
-  GGSTAMPA in '..\..\..\go_1010\src_101002\GGSTAMPA.pas' {STAMPA},
+  DMARC in '..\..\..\go_1011\src\DMARC.pas' {ARC: TDataModule},
+  GGLOGIN in '..\..\..\go_1011\src\GGLOGIN.pas' {LOGIN},
+  GGBASE in '..\..\..\go_1011\src\GGBASE.pas' {BASE},
+  GGFORMBASE in '..\..\..\go_1011\src\GGFORMBASE.pas' {FORMBASE},
+  GGSTAMPA in '..\..\..\go_1011\src\GGSTAMPA.pas' {STAMPA},
   DMCLOUD in 'DMCLOUD.pas' {CLOUD: TDataModule},
   FRMDITTA in 'FRMDITTA.pas' {DITTA: TFrame},
   GGMENU in 'GGMENU.pas' {MENUGG};
